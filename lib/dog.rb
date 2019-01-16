0efb9e1e28edd7c4a37df7f53389f3b17e1a70d0
tylerselldesign <tylerselldesign@gmail.com>
2019-01-16 06:32:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,10 +68,10 @@ class Dog
   
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM dogs WHERE id = ? LIMIT 1
+      SELECT * FROM dogs WHERE name = ? LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, id).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
