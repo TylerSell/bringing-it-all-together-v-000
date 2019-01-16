43e0c6f735f065427a690ae7baecab2d925179a5
tylerselldesign <tylerselldesign@gmail.com>
2019-01-16 06:32:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,11 @@ class Dog
   end
   
   def self.new_from_db(row)
-    
+    dog = self.new 
+    dog.id = row[0]
+    dog.name = row[1]
+    dog.breed = row[2]
+    dog 
   end
   
   def self.find_by_name
