245b5db4f7b957872db79b07490b10b0a4f04e8a
tylerselldesign <tylerselldesign@gmail.com>
2019-01-16 06:32:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,8 +66,14 @@ class Dog
     dog 
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ? LIMIT 1
+    SQL
     
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def update
